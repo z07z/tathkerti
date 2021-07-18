e2ac783ba73b0f4f7570dc0b9361f924e9f6eb31
Khaled Alzahrani <z07z@live.com>
2021-07-18 15:08:21 +0000

    first commit

---
+++ config/database.js
@@ -0,0 +1,22 @@
+const fs = require('fs')
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        host: env('DATABASE_HOST'),
+        srv: env.bool('DATABASE_SRV', false),
+        port: env.int('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
+      },
+      options: {
+       ssl: true,
+       sslCA: env('DATABASE_SSL'),
+      },
+    },
+  },
+});
+
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6ed6b84c-d5ac-4d20-a896-385a1cb7ba91'
+};
